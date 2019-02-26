99aa3484b0c9d0b704930967268b177ec090f3d1
Ivan Balashov Desktop <ivan.programmer@bk.ru>
2019-02-26 18:02:44 +0000

    2 : Add a script to search for photos by hashtag

--- index.php
+++ index.php
@@ -7,15 +7,17 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
 
     <title>Найди по хештегу</title>
   </head>
   <body>
-    <form class = "container">
+    <form class = "container" method="POST">
       <br />
-       <input type="text" class="form-control" placeholder="Введите хэш-тег">
+      <label id = "tag">Введите слово без пробелов</label>
+       <input type="text" name = "tag" class="form-control" placeholder="Введите хэш-тег без решетки">
        <br />
-       <button type="submit" class="btn btn-primary mb-2">Найти фоточку в инсте</button>
+       <button type="submit" name = "search" class="btn btn-primary mb-2">Найти фоточку в инсте</button>
       <br />
     </form>
     <!-- Optional JavaScript -->
@@ -25,3 +27,34 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
+
+<?php
+if(isset($_POST['search']))
+{
+require __DIR__ . '/vendor/autoload.php';
+$instagram = new \InstagramScraper\Instagram();
+// Let's look at $media
+$tag = $_POST['tag'];
+$tag = str_replace(' ','',$tag);
+$tag = str_replace('#','',$tag);
+$medias = $instagram->getCurrentTopMediasByTagName($tag);
+
+for($i = 0; $i < count($medias); $i++)
+{
+$media = $medias[$i];
+$im = $media->getImageHighResolutionUrl();
+echo '
+  <div class = "container">
+  <div class="card cardos">
+  <img src="'.$im.'" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+    <a href="'.$im.'" class="btn btn-primary">Открыть фото в полном размере</a>
+  </div>
+  </div>
+  </div>
+';
+}
+}
+?>
